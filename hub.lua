6cec00b76298013b88c8dea650fcdd6d4f88900d
Internl <135014218+Internl@users.noreply.github.com>
2024-06-18 13:25:34 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,6 +1,1 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Internl/abc/main/hubtop.lua"))()
-        end)
-task.spawn(function()
-    loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/PetSimulator99/main/redz9999.lua"))()", true))()
-        end)
+loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/PetSimulator99/main/redz9999.lua"))()", true))()
